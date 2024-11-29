ff5b0156465ce57ec18567129315d701f7eabbad
Alona Chuchupalova <a.s.chuchupalova@student.khai.edu>
2024-11-29 10:06:25 +0200

    FitnesCenterTest

--- FitnessCenterProjectTests/TrainerTests.cs
+++ FitnessCenterProjectTests/TrainerTests.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FitnessCenterProject;
 
 namespace FitnessCenterProjectTests
 {
-    internal class TrainerTests
+    [TestClass]
+    public class TrainerTests
     {
+        public void Salary_ShouldBeSetCorrectly()
+        {
+            // Arrange
+            var trainer = new Trainer
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                Age = 35,
+                Nationality = "USA",
+                Salary = 2000
+            };
+
+            // Assert
+            Assert.Equal(2000, trainer.Salary);
+            Assert.Equal("Jane", trainer.FirstName);
+            Assert.Equal(35, trainer.Age);
+        }
     }
+
 }
